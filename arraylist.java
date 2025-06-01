b1a3c91a219000d6f7d458329767595ffd41eb53
tasim552 <alsaharia74@gmail.com>
2025-06-01 23:43:39 +0600

    Add files via upload

---
+++ arraylist.java
@@ -0,0 +1,20 @@
+package arraylist;
+
+import.java.util.Arraylist;
+
+import.java.util.Arrays;
+
+public class Number1 {
+
+public static void main(String[] args) {
+
+System.out.println("Enter the number");
+
+String[]arry={"2","3","4","5","8"};
+
+ArrayList <String> arrayList =new ArrayList<>(Arrays.asList(array));
+
+System.out.println(arrayList);
+}
+}
+
